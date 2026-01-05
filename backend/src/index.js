6b110c841403e6ee9ed8646504b9724a59520177
LazzouziYoussefEtu <youssef.lazzouzi@etu.uae.ac.ma>
2026-01-05 12:21:43 +0100

    Add Clerk middleware and auth protection middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
+
+import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import adminRoutes from "./routes/admin.route.js";
 import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import statsRoutes from "./routes/stats.route.js";
-import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -14,6 +16,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(clerkMiddleware());
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
